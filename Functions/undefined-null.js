6a6f282a97cc12680a9f9e1665d99b223039fa59
Nathan Cobb <nathanrcobb@gmail.com>
2022-10-21 21:01:01 -0700

    Undefined and null

---
+++ Functions/undefined-null.js
@@ -0,0 +1,25 @@
+let name
+
+name = 'Nate'
+
+if (name === undefined) {
+    console.log('Please provide a name.')
+} else {
+    console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as function default return value
+let square = function (num) { 
+    console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+// Null as assigned value
+let age = 32
+
+age = null
+
+console.log(age)
