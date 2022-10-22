adadc3f66eefe8d45a597b31e4034b71ed7d4953
Nathan Cobb <nathanrcobb@gmail.com>
2022-10-21 17:22:25 -0700

    Temperature conversion

---
+++ temp-conversion.js
@@ -0,0 +1,5 @@
+let fahrenheit = 50
+let celsius = (fahrenheit - 32) * 5/9
+let kelvin = ((fahrenheit - 32) / 1.8) + 273.15
+
+console.log(`Fahrenheit: ${fahrenheit} F\nCelsius: ${celsius} C\nKelvin: ${kelvin}k`)
