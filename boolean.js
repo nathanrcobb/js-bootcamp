49a033c0dd2ad7d50280b810dcfad83b9f3e4b10
Nathan Cobb <nathanrcobb@gmail.com>
2022-10-21 17:39:48 -0700

    Booleans

---
+++ boolean.js
@@ -0,0 +1,18 @@
+// === - equality operator
+// !== - inequality operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+let temp = 32
+let isFreezing = temp <= 32
+
+// console.log(isFreezing)
+
+// Challenge
+let age = 65
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(`Child: ${isChild}\nSenior: ${isSenior}`)
