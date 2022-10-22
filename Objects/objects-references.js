73af919a1061c7a47b2051aa45c8a9986c2946ff
Nathan Cobb <nathanrcobb@gmail.com>
2022-10-22 16:44:36 -0700

    Object references challenge

--- Objects/objects-references.js
+++ Objects/objects-references.js
@@ -8,6 +8,24 @@ let addExpense = function (account, amount) {
     account.expenses += amount
 }
 
-addExpense(myAccount, 2.50)
+let addIncome = function (account, income) {
+    account.income += income
+}
 
-console.log(myAccount)
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+let getAccountSummary = function (account) {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. ` +
+           `$${account.expenses} in expenses.`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
